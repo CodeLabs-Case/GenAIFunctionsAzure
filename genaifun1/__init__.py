c619ae895b0c811bc0f1793fff4d1bff6ba25359
Davis <a.b.davis@accenture.com>
2023-06-27 16:45:13 -0400

    Deploy fun1 9

--- genaifun1/__init__.py
+++ genaifun1/__init__.py
@@ -7,13 +7,14 @@ import azure.functions as func
 import datetime
 
 connection_string = 'DefaultEndpointsProtocol=https;AccountName=genaiazurefun;AccountKey=m8WBdyeSy8tctCi/4phepBAcQhy0VhtiN+3nWsl0/w+F00HesbMGb8bz6KuS073l4kS3S6Wif4+L+AStyeZ+Qg==;EndpointSuffix=core.windows.net'
-container_name = 'container1'
+input_container_name = 'container1'
+output_container_name = 'container1output'
 
 # Create a BlobServiceClient object
 blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 
 # Get a reference to the container
-container_client = blob_service_client.get_container_client(container_name)
+container_client = blob_service_client.get_container_client(input_container_name)
 
 def main(myblob: func.InputStream):
 
@@ -29,6 +30,6 @@ def main(myblob: func.InputStream):
     file_name=f'{myblob.name}_output_{timestamp}.csv'
 
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-    container_client = blob_service_client.get_container_client(container_name)
+    container_client = blob_service_client.get_container_client(output_container_name)
     blob_client = container_client.get_blob_client(file_name)
     blob_client.upload_blob(context, overwrite=True)
