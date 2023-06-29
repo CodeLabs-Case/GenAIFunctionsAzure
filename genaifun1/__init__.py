8d0d962f17d298097874ada32c6e70da25959cf8
Davis <a.b.davis@accenture.com>
2023-06-29 08:40:05 -0400

    Transformation Testing 1

--- genaifun1/__init__.py
+++ genaifun1/__init__.py
@@ -5,6 +5,7 @@ import urllib.request
 from azure.storage.blob import BlobServiceClient
 import azure.functions as func
 import datetime
+import pandas as pd
 
 connection_string = 'DefaultEndpointsProtocol=https;AccountName=genaiazurefun;AccountKey=m8WBdyeSy8tctCi/4phepBAcQhy0VhtiN+3nWsl0/w+F00HesbMGb8bz6KuS073l4kS3S6Wif4+L+AStyeZ+Qg==;EndpointSuffix=core.windows.net'
 input_container_name = 'container1'
@@ -18,21 +19,22 @@ container_client = blob_service_client.get_container_client(input_container_name
 
 def main(myblob: func.InputStream):
 
+    # Basic logging
     logging.info(f"Python blob trigger function processed blob \n"
                  f"Name: {myblob.name}\n"
                  f"Blob Size: {myblob.length} bytes")
     
+    # The value of myblob.name is of the pattern: <container_name/file_name>
+    blob_url = f"https://genaiazurefun.blob.core.windows.net/{myblob.name}"
 
-    # with urllib.request.urlopen(f"https://genaiazurefun.blob.core.windows.net/container1/{myblob.name}") as response:
-    #     context = response.read().decode('utf-8')
-
-    blob_url = [
-        f"https://genaiazurefun.blob.core.windows.net/{myblob.name}",
-        "https://genaiazurefun.blob.core.windows.net/container1/data202306271438.csv"
-    ]
-
-    with urllib.request.urlopen(blob_url[0]) as response:
+    # Use urllib to request the file, using blob_url
+    with urllib.request.urlopen(blob_url) as response:
         context = response.read().decode('utf-8')
+
+    # Convert the context into a data-frame for transformations
+    df = pd.DataFrame([context], columns=['product','review'])
+
+    logging.info(f"\n===============\nDataframe Contents:\n{df}\n===============\n")
     
     timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M")
     file_name=f"output_{timestamp}.csv"
