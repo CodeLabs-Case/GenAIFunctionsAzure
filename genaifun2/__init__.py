c6ffa6feb7a9ae32cdbb1158e4cb40b4d6a7cbcb
Davis <a.b.davis@accenture.com>
2023-07-06 10:40:59 -0400

    Function2 6

--- genaifun2/__init__.py
+++ genaifun2/__init__.py
@@ -11,6 +11,8 @@ import os
 import re
 import time
 
+
+
 connection_string = 'DefaultEndpointsProtocol=https;AccountName=genaiazurefun;AccountKey=m8WBdyeSy8tctCi/4phepBAcQhy0VhtiN+3nWsl0/w+F00HesbMGb8bz6KuS073l4kS3S6Wif4+L+AStyeZ+Qg==;EndpointSuffix=core.windows.net'
 input_container_name = 'container2'
 output_container_name = 'container2output'
@@ -20,6 +22,60 @@ blob_service_client = BlobServiceClient.from_connection_string(connection_string
 
 # Get a reference to the container
 container_client = blob_service_client.get_container_client(input_container_name)
+
+
+
+def outline(prompt):
+    response = openai.ChatCompletion.create(
+        model = 'gpt-3.5-turbo-16k',
+        messages = [
+            {'role': 'system','content':f"{prompt}"}]
+    )
+    outline = response.choices[0].message['content'].strip()
+
+    return outline
+
+
+
+def expansion(outline):
+    response = openai.ChatCompletion.create(
+        model = 'gpt-3.5-turbo-16k',
+        messages = [
+            {'role': 'system','content':f"{outline}.\n How many numbered points are in the above outline, do not include sub-points (letters)?"}]
+    )
+    n_sentence = response.choices[0].message['content'].strip()
+
+    # Get 'n' from the sentence
+    number = re.findall(r'\d+', n_sentence)
+    n = int(number[0])
+    logging.info(f"There are {n} points in the outline")
+
+    # Loop over each point in the outline, expand it, append the results to the string, then output to the final file
+    final_string = ""
+    for i in range(n):
+        i = i + 1
+        response = openai.ChatCompletion.create(
+        model = 'gpt-3.5-turbo-16k',
+        messages = [
+            {'role': 'system','content':f"{outline}.\n Expand on point {i}, don't include your own response like 'certainly'"}]
+        )
+        final_string += "\n" + response.choices[0].message['content'].strip()
+    
+    return final_string
+
+
+
+def facts(outline):
+    response = openai.ChatCompletion.create(
+        model = 'gpt-3.5-turbo-16k',
+        messages = [
+            {'role': 'system','content':f"Context: {outline}\nGive me a list of facts stated in the above context"}]
+    )
+    facts = response.choices[0].message['content'].strip()
+
+    return facts
+
+
 
 def main(myblob: func.InputStream):
     # Get API key
@@ -43,39 +99,16 @@ def main(myblob: func.InputStream):
 
 
     ### OPENAI
-    # Get the outline, re-use it 'n' times for each point
     openai.api_key = openai_api_key
-    response = openai.ChatCompletion.create(
-        model = 'gpt-3.5-turbo-16k',
-        messages = [
-            {'role': 'system','content':f"{prompt}"}]
-    )
-    outline = response.choices[0].message['content'].strip()
+    
+    # Outline: Get the outline, re-use it 'n' times for each point
+    outline = outline(prompt)
+    
+    # Expansion: Get the number of points in the outline 'n'. Commented out due to rate limit issues.
+    #expansion = expansion(outline)
 
-    # Get the number of points in the outline 'n'
-    response = openai.ChatCompletion.create(
-        model = 'gpt-3.5-turbo-16k',
-        messages = [
-            {'role': 'system','content':f"{outline}.\n How many numbered points are in the above outline, do not include sub-points (letters)?"}]
-    )
-    n_sentence = response.choices[0].message['content'].strip()
-
-    # Get 'n' from the sentence
-    number = re.findall(r'\d+', n_sentence)
-    n = int(number[0])
-    logging.info(f"There are {n} points in the outline")
-
-    # Loop over each point in the outline, expand it, append the results to the string, then output to the final file
-    final_string = ""
-    for i in range(n):
-        i = i + 1
-        response = openai.ChatCompletion.create(
-        model = 'gpt-3.5-turbo-16k',
-        messages = [
-            {'role': 'system','content':f"{outline}.\n Expand on point {i}, don't include your own response like 'certainly'"}]
-        )
-        final_string += "\n" + response.choices[0].message['content'].strip()
-        time.sleep(20)
+    # Facts: Get the facts in the generated content
+    facts = facts(outline)
 
 
 
@@ -86,4 +119,4 @@ def main(myblob: func.InputStream):
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
     container_client = blob_service_client.get_container_client(output_container_name)
     blob_client = container_client.get_blob_client(file_name)
-    blob_client.upload_blob(final_string, overwrite=True)
+    blob_client.upload_blob(facts, overwrite=True)
